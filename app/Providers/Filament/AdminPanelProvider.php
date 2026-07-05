260936368faec696401e22cd9b001e8d85452097
Ahmed Abdelalim <ahmed.abulaleem@uniparticle.com>
2026-07-05 13:25:40 +0300

    Restore brand logo and favicon settings in AdminPanelProvider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,11 +29,10 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
-            // ->brandLogo(asset('images/logo.png'))   // ✅ sets logo in login page & header
-            ->brandLogoHeight('3rem')              // optional: adjust size
-            // ->brandName('Nitaqat Admin Panel')              // optional: text beside logo
-            ->brandName("N@I%T&a)q**at")
-            // ->favicon(asset('images/logo.png'))   // ✅ sets browser tab icon
+            ->brandLogo(asset('images/logo.png'))   // ✅ sets logo in login page & header
+            ->brandLogoHeight('Nitaqat')              // optional: adjust size
+            ->brandName("Nitaqat Admin Panel")              // optional: text beside logo
+            ->favicon(asset('images/logo.png'))   // ✅ sets browser tab icon
             ->colors([
                 'primary' => Color::Amber,
             ])
